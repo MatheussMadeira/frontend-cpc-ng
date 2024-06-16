0b8174be4e2ecdc9c0c460967b89d41840184cc8
matheusmadeira <fumacammadeira@gmail.com>
2024-06-15 23:38:48 -0300

    feat: pagina eventos feita
    only front sem linkagem

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,12 +1,69 @@
-import { Container, TitlePrincipal, DivPrincipal } from "./styleHome";
+import {
+  Container,
+  TitlePrincipal,
+  DivPrincipal,
+  TitleInput,
+  Form,
+  DivInputs,
+  StyledImg,
+  StyledPen,
+  StyledUrl,
+  DivSelect,
+  SelectDe,
+  OptionsS,
+} from "./style";
 import Header from "../../components/header/header";
+import { InputD } from "../../components/common/styleInput";
+import { ButtonDefault } from "../../components/common/styleButton";
+import EditEventos from "../../components/editEventos/editEventos";
 
 function Home() {
   return (
     <Container>
       <Header></Header>
       <DivPrincipal>
-        <TitlePrincipal>ADICIONAR NOVO EVENTO</TitlePrincipal>
+        <Form>
+          <TitlePrincipal marginTop="20px">
+            ADICIONAR NOVO EVENTO
+          </TitlePrincipal>
+          <DivInputs>
+            <TitleInput>
+              Título
+              <StyledPen />
+            </TitleInput>
+            <InputD></InputD>
+            <TitleInput>
+              Imagem
+              <StyledImg />
+            </TitleInput>
+            <InputD></InputD>
+            <TitleInput>
+              Descrição
+              <StyledUrl />
+            </TitleInput>
+            <InputD></InputD>
+            <DivSelect>
+              <TitleInput>Categorias:</TitleInput>
+              <SelectDe placeholder="Opções de categoria:">
+                <OptionsS value="red">Vermelho</OptionsS>
+                <OptionsS value="blue">Azul</OptionsS>
+                <OptionsS value="green">Verde</OptionsS>
+                <OptionsS value="yellow">Amarelo</OptionsS>
+                <OptionsS value="black">Preto</OptionsS>
+              </SelectDe>
+            </DivSelect>
+          </DivInputs>
+          <ButtonDefault
+            marginRight="0px"
+            borderColor="none"
+            backgroundColor="#F19709"
+            marginTop="20px"
+          >
+            Enviar
+          </ButtonDefault>
+        </Form>
+        <TitlePrincipal marginTop="50px">GERENCIAR EVENTOS</TitlePrincipal>
+        <EditEventos />
       </DivPrincipal>
     </Container>
   );
